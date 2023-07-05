d8cc866944e8119d98aa6a51df6c0406a7f79745
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2023-07-05 17:23:38 +0700

    tesst

--- MainComponents/Views/ItemComponent/CarCards/CarCard.js
+++ MainComponents/Views/ItemComponent/CarCards/CarCard.js
@@ -1,23 +1,28 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import CarCardCss from './CarCardCss'
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import CarCardCss from "./CarCardCss";
 
 const CarCard = (props) => {
   return (
-    <View style={CarCardCss.cardView}>
-      <Image
-          source={{ uri: props.imguri }}
-          style={CarCardCss.img}
-      />
-      <View style={CarCardCss.textView}>
-        <Text style={CarCardCss.text}>{props.tenxe}</Text>
-        <Text style={CarCardCss.text}>{props.gia} đ<Text style={{color:'gray', fontSize: 15, fontWeight:'normal'}}>/ Ngày</Text></Text>
+    <>
+      <View style={CarCardCss.cardView}>
+        <Image source={{ uri: props.imguri }} style={CarCardCss.img} />
+        <View style={CarCardCss.textView}>
+          <Text style={CarCardCss.text}>{props.tenxe}</Text>
+          <Text style={CarCardCss.text}>
+            {props.gia} đ
+            <Text style={{ color: "gray", fontSize: 15, fontWeight: "normal" }}>
+              / Ngày
+            </Text>
+          </Text>
+        </View>
+        <TouchableOpacity style={CarCardCss.touch}>
+          <Text style={{ color: "white", fontSize: 17 }}>Chi tiết {">"}</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={CarCardCss.touch}>
-          <Text style={{color: 'white', fontSize: 17}}>Chi tiết {'>'}</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+      <View></View>
+    </>
+  );
+};
 
-export default CarCard
+export default CarCard;
