07d6db7263a3593d7df4b6813c1e80e9e9e901c2
Minh <kespikun@gmail.com>
2023-07-28 14:34:48 +0700

    Update UserInfo

--- urlAPI.js
+++ urlAPI.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const url = "https://26ac-2402-800-63a7-e5ea-ad09-40a9-f3b4-bf96.ngrok-free.app"
+const url = "https://22f7-203-205-32-58.ngrok.io"
   
 export default url
